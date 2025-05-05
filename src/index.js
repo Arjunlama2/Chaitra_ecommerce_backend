3d4bc5b1652f8428294625cc5d55a2a76306167f
Arjunlama2 <instructorarjun1@gmail.com>
2025-05-05 16:31:54 +0545

    setup

---
+++ src/index.js
@@ -0,0 +1,11 @@
+const express=require("express")
+const dotenv=require("dotenv")
+dotenv.config()
+
+
+const port=process.env.PORT
+const app=express()
+
+app.listen(port,()=>{
+    console.log( `Server is listening to port ${port}`)
+})
