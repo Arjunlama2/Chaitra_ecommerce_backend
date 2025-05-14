3d5dee93f38f4a8193d151f256a20e18becc4af2
Arjunlama2 <instructorarjun1@gmail.com>
2025-05-14 16:11:06 +0545

    sorts and filter

--- src/conttoller/product.contoller.js
+++ src/conttoller/product.contoller.js
@@ -10,6 +10,46 @@ const productSchema = Joi.object({
   createdBy: Joi.string(),
 });
 
+const getProducts = async (req, res, next) => {
+  try {
+    let sort = req.query.sort || "dateDesc";
+    let priceFrom = parseFloat(req.query.priceFrom) || 0;
+    let priceTo = parseFloat(req.query.priceTo) || 9999999999;
+    let perPage = parseInt(req.query.perPage) || 5;
+    let page = parseInt(req.query.page) || 1;
+
+    let sortBy = {
+      createdAt: -1,
+    };
+
+    if (sort == "priceAsc") {
+      sortBy = { price: 1 };
+    } else if (sort == "priceDesc") {
+      sortBy = { price: -1 };
+    } else if (sort == "titleAsc") {
+      sortBy = { title: 1 };
+    } else if (sort == "titleDesc") {
+      sortBy = { title: -1 };
+    }
+    
+    let productFilter = {
+      name: new RegExp(req.query.search, "i"),
+     
+      
+      $and: [{ price: { $gte: priceFrom } }, { price: { $lte: priceTo } }],
+    };
+    const product = await Product.find(productFilter)
+      .populate("category")
+      .sort(sortBy)
+      .skip((page-1)*perPage)
+      .limit(perPage)
+      .populate("createdBy", { password: 0 ,role:0});
+    res.status(200).send(product);
+  } catch (err) {
+    next(err);
+  }
+};
+
 const createProduct = async (req, res, next) => {
   try {
     const { error, value } = productSchema.validate(req.body, {
@@ -26,17 +66,7 @@ const createProduct = async (req, res, next) => {
   }
 };
 
-const getProducts=async(req,res,next)=>{
-    try{
-        const product=await Product.find().populate("category").populate("createdBy",{password:0})
-        res.status(200).send(product.reverse())
-    }catch(err){
-        next(err)
-    }
-}
-
-module.exports={
-    createProduct,
-    getProducts
-    
-}
+module.exports = {
+  createProduct,
+  getProducts,
+};
